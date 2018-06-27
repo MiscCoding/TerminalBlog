02a1eee94411bbc649e97a9bd4e95085d5284a72
neogeoss <neogeo-s@hanmail.net>
2018-06-28 00:17:58 +0900

    Menu added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from src.database import Database
+from src.menu import Menu
 from src.model.blog import Blog
 from src.model.post import Post
 
@@ -7,24 +8,27 @@ __author__ = 'jslvtr'
 Database.initialize()
 
 '''
-#post = Post(blog_id = "123",
-           ## title="Another great post",
-           ## content="123432dsfasdf",
-            ##author="Jose")
+post = Post(blog_id = "123",
+            title="Another great post",
+            content="123432dsfasdf",
+            author="Jose")
 
 posts = Post.from_blog('123')
 #post.save_to_mongo()
 for post in posts:
-    print(post)  '''
-
+    print(post)'''
+'''
 blog = Blog(author ="Jose",
             title = "Sample title",
             description="Sample description")
 
 blog.new_post()
 
-blog.save_to_mongo(blog.id)
+blog.save_to_mongo()
 
 from_database = Blog.from_mongo(blog.id)
 
-print(blog.get_posts())
+print(blog.get_posts())'''
+
+menu = Menu()
+menu.run_menu()
